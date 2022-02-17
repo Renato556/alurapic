588561fc513c8fbb72866fe92a7e5f9013a3b0fc
Renato Cabral Rodrigues de Souza <CIANDT\renatocrds@lnb035014blz.ciandt.global>
2022-02-17 15:56:47 -0300

    novo módulo usando *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  photos = [
+    {
+      url: 'https://www.dicaspetz.com.br/wp-content/uploads/2019/04/quantos-anos-vive-um-gato.jpg',
+      description: 'Gato'
+    },
+    {
+      url: 'https://saude.abril.com.br/wp-content/uploads/2018/12/cachorro-livro.png',
+      description: 'Cachorro'
+    },
+    {
+      url: 'https://s3.amazonaws.com/media.wikiaves.com.br/images/221/122761_f41b46dfaa37205e41c1a190ceb2ebf8.jpg',
+      description: 'Papagaio'
+    }
+  ];
 }
