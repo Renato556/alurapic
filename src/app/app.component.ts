9b8352662b46d08020734ea4a6c1e6824bfef7d9
Renato Cabral Rodrigues de Souza <CIANDT\renatocrds@lnb035014blz.ciandt.global>
2022-02-17 17:25:58 -0300

    web API implementada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from './photos/photo/photo.service';
 
 @Component({ //decorator
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  photos = [
-    {
-      url: 'https://www.dicaspetz.com.br/wp-content/uploads/2019/04/quantos-anos-vive-um-gato.jpg',
-      description: 'Gato'
-    },
-    {
-      url: 'https://saude.abril.com.br/wp-content/uploads/2018/12/cachorro-livro.png',
-      description: 'Cachorro'
-    },
-    {
-      url: 'https://s3.amazonaws.com/media.wikiaves.com.br/images/221/122761_f41b46dfaa37205e41c1a190ceb2ebf8.jpg',
-      description: 'Papagaio'
-    }
-  ];
+export class AppComponent implements OnInit{
+  photos: any[] = [];
+  
+  constructor(private photoService: PhotoService) { }
+    
+  ngOnInit(): void {
+    
+    this.photoService
+      .listFromUser('flavio')
+      .subscribe(photos => this.photos = photos); 
+  }
 }
+
