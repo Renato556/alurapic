32340fa09f1940595fb950cd8d1f4bf9bf4eebb7
Renato Cabral Rodrigues de Souza <CIANDT\renatocrds@lnb035014blz.ciandt.global>
2022-02-17 15:18:12 -0300

    bootstrap adicionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({ //decorator
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
+  title = 'Alurapic';
+  description = 'Gato';
+  url = 'https://www.dicaspetz.com.br/wp-content/uploads/2019/04/quantos-anos-vive-um-gato.jpg';
 }
